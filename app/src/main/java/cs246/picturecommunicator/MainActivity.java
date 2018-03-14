bfac46eef111e24dc527ea68d7d967f62a86d62d
landcamp <landcamp@byui.edu>
2018-03-14 12:10:12 -0600

    Added another java file for my List Sub Menu part of the project.

--- app/src/main/java/cs246/picturecommunicator/MainActivity.java
+++ app/src/main/java/cs246/picturecommunicator/MainActivity.java
@@ -3,11 +3,19 @@ package cs246.picturecommunicator;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -52,5 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
 }
 
