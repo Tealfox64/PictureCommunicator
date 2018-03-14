30f505064514edd7366dafc89742d1f05410aa4b
anthony <campbellanthony04@gmail.com>
2018-03-14 13:18:15 -0600

    Rough menu. Have some ideas for changing aesthetics.

--- app/src/main/java/cs246/picturecommunicator/MainActivity.java
+++ app/src/main/java/cs246/picturecommunicator/MainActivity.java
@@ -3,19 +3,11 @@ package cs246.picturecommunicator;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -60,7 +52,5 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
-
 }
 
