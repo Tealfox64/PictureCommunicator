a6c08af8b78619cbc778f2f4137136b0d29b0901
Matthias Bruveleit <tealfox64@gmail.com>
2018-03-23 15:32:05 -0600

    Created text file for images. Moved image folder to resources

--- app/src/main/java/Picture.java
+++ app/src/main/java/Picture.java
@@ -2,11 +2,36 @@
  * Created by mlbru on 3/21/2018.
  */
 
+/**
+ *
+ */
 public class Picture {
+    /**
+     * A class to store individual data for each image file. Includes:
+     * <ul>
+     *     <li>String filename: holds the actual filename of an image.</li>
+     *     <li>String label: the name of the image, can be displayed with it.</li>
+     *     <li>String category: the category the image falls under</li>
+     * </ul>
+     */
     protected String filename;
     protected String label;
     protected String category;
 
+    /**
+     * Picture (default constructor) sets all variables to ""
+     */
+    public Picture() {
+        this.filename = "";
+        this.label = "";
+    }
+
+    /**
+     * Picture (non-default constructor) accepts 3
+     * @param filename
+     * @param label
+     * @param category
+     */
     public Picture(String filename, String label, String category) {
         this.filename = filename;
         this.label = label;
